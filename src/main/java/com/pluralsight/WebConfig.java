7747fae7c076e879dcd03dea042c308bdf6d4d4e
Gergely Szilagyi <gergely.szilagyi@ericsson.com>
2017-01-24 16:30:25 +0100

    Configuring Controller with web.xml

--- src/main/java/com/pluralsight/WebConfig.java
+++ src/main/java/com/pluralsight/WebConfig.java
@@ -1,11 +1,13 @@
 package com.pluralsight;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 //during spring load this configuration will be read
 @Configuration	//use this annotation(plus other config annotations) instead of web.xml, if I want I can still create web.xml, but the Servlet 3+ specification negates it.
 @EnableWebMvc //it is in place of the servlet-config.xml in MVC 3, In that file we had the component scanner and beans e.g: for Internal view resolvers
+@ComponentScan(basePackages = "com.pluralsight")
 public class WebConfig {
 
 }
